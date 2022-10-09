b1b27dd0edbd3a040b1d2153db5ca75251898618
RAFAL RAPCEWICZ <rafal@RAFALs-MacBook-Air.local>
2022-10-09 15:30:36 +0200

    cleaning apps

--- frontend/src/SagaApp.tsx
+++ frontend/src/SagaApp.tsx
@@ -1,7 +1,22 @@
 import { Provider } from "react-redux";
 import store from "./store/saga-store";
+import logo from './logo.svg';
+import { Users } from './components/UsersSaga';
 import "./index.scss";
-import App from "./App";
+
+import './App.scss';
+
+// 2 ways of using async thunk actions a) with "root" reducer & b) with "local" useReducer
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <Users />
+            </header>
+        </div>
+    );
+}
 
 // this code is normaly part of index.tsx (here wrapper to cusmomize store)
 function SagaApp() {
--- frontend/src/ThunkApp.tsx
+++ frontend/src/ThunkApp.tsx
@@ -1,7 +1,25 @@
 import { Provider } from "react-redux";
 import store from "./store/store";
+import logo from './logo.svg';
 import "./index.scss";
-import App from "./App";
+
+// Users & UsersUseThunkReducer are working with ThunkApp
+// import { Users } from './components/Users';
+import { Users } from './components/UsersUseThunkReducer';
+
+import './App.scss';
+
+// 2 ways of using async thunk actions a) with "root" reducer & b) with "local" useReducer
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <Users />
+            </header>
+        </div>
+    );
+}
 
 // this code is normaly part of index.tsx (here wrapper to cusmomize store)
 function ThunkApp() {
